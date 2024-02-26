0a420dd5df07fab2bd8a17f230ccf118dc6fe3be
Shashank S. Surjekar <143866820+shashanks-06@users.noreply.github.com>
2024-02-26 23:18:39 +0530

    Create server.js

---
+++ 1.Introduction_to_ExpressJS/1.Build_Your_Own_Framework/1.Waffle/server.js
@@ -0,0 +1,27 @@
+import Waffle from "./lib";
+
+const server = new Waffle();
+
+server.get("/", (res) => {
+  res.send("Hello from Waffle!");
+});
+
+server.get("/about", (res) => {
+  res.send("This is the About Page!");
+});
+
+server.get("/api", (res) => {
+  res.json({
+    Name: "Sherlock Holmes",
+    Occupation: "Detective",
+    City: "London",
+  });
+});
+
+server.get("/blog", (res) => {
+  res.html("blog");
+});
+
+server.start(3000, () => {
+  console.log("Server is running on port 3000");
+});
