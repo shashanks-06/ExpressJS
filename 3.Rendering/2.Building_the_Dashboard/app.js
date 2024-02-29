1bf291b0e1c42a64fcc9d4fec766308ac59569fe
Shashank S. Surjekar <143866820+shashanks-06@users.noreply.github.com>
2024-03-01 00:09:19 +0530

    Create app.js

---
+++ 3.Rendering/2.Building_the_Dashboard/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
+
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use("/assets", express.static(join(__dirname, "public")));
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.send("<h1>BlogChef</h1>");
+});
+
+app
+  .get("/admin/login", (req, res) => res.render("login"))
+  .post("/admin/login", (req, res) => res.redirect("/admin/dashboard"));
+
+app.get("/admin/dashboard", (req, res) => res.render("dashboard"));
+app.get("/admin/logout", (req, res) => res.redirect("/admin/login"));
+
+app.listen(3000, () => console.log("Blog Chef is cooking on port 3000"));
